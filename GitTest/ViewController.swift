db387851e522e0686ffe1637e63c95d9d055b6d4
Salam El Kadri <salam.elkadri@montymobile.com>
2022-01-14 13:11:54 +0200

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by WSLOSX121 on 14/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
